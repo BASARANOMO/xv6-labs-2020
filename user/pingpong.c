5423997a69afd8a1dd9e2520e944f09251fa3c64
M. ZHANG <39518094+BASARANOMO@users.noreply.github.com>
2021-06-03 12:11:42 +0800

    fix bug in pingpong program

--- user/pingpong.c
+++ user/pingpong.c
@@ -8,23 +8,40 @@ main(int argc, char *argv[])
   char buf[1];
   int n;
   int pid;
-  int p[2];
-  pipe(p);
+  int p1[2];
+  int p2[2];
+  pipe(p1);
+  pipe(p2);
   if (fork() == 0) {
       pid = getpid();
-      n = read(p[1], buf, 1);
+      close(p1[1]);
+      close(p2[0]);
+      n = read(p1[0], buf, 1);
       if (n) {
           printf("%d: received ping\n", pid);
+      } else {
+          fprintf(2, "usage: pingpong parent--->child read error!\n");
+          exit(1);
       }
-      write(p[0], buf, 1);
+      if (!write(p2[1], buf, 1)) {
+          fprintf(2, "usage: pingpong child--->parent write error!\n");
+          exit(1);
+      }
       exit(0);
   } else {
       pid = getpid();
-      write(p[1], buf, 1);
-      wait((int *) 0);
-      n = read(p[0], buf, 1);
+      close(p1[0]);
+      close(p2[1]);
+      if (!write(p1[1], buf, 1)) {
+          fprintf(2, "usage: pingpong parent--->child write error!\n");
+          exit(1);
+      }
+      n = read(p2[0], buf, 1);
       if (n) {
           printf("%d: received pong\n", pid);
+      } else {
+          fprintf(2, "usage: pingpong child--->parent read error!\n");
+          exit(1);
       }
       exit(0);
   }
